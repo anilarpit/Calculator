f93f0a5cbb913c3785c126787c28e0c2921281cc
anilarpit <anilarpit07@gmail.com>
2022-08-20 19:52:31 -0400

    update in button arrangements & output format

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Working")
 
-function sum (x, y) {
-    let add = x+y;
+function sum(x, y) {
+    let add = x + y;
     console.log(add);
     return add;
 }
@@ -24,52 +24,58 @@ function sub(x, y) {
     return out;
 }
 
+function print_nat(numid) {
+    let out = document.getElementsByClassName('output')[0];
+    let output_area = out.innerHTML;
+    console.log(output_area)
+    let num = numid[numid.length-1];
+    console.log
+    if (output_area == "0") {
+        output_area = num;
+    } else {
+        output_area = output_area + num;
+    }
+    out.innerHTML = output_area;
+}
+
+function print_zero() {
+    let out = document.getElementsByClassName('output')[0];
+    let print_area = out.innerHTML;
+    if(print_area[print_area.length - 2] == "/") {
+        alert("Answer: Undefined\nNote:You have tried to divide by 0")
+        return
+    }
+    if (out.innerHTML != "0") {
+        out.innerHTML = out.innerHTML + 0;
+    }
+}
+
+function solve(click_id) {
+    let out = document.getElementsByClassName('output')[0];
+    let 
+}
+
 function printout(click_id) {
     console.log(click_id)
     let button_clicked = document.getElementById(click_id);
-    let out =document.getElementsByClassName('output')[0];
-    if(click_id == "del") {
+    let out = document.getElementsByClassName('output')[0];
+    if (click_id == "del") {
         console.log("button pressed")
         out.innerHTML = "";
-    } else if (click_id == "one") {
-        out.innerHTML = out.innerHTML + "1";
-    } else if (click_id == "two") {
-        out.innerHTML = out.innerHTML + "2";
-    } else if (click_id == "three") {
-        out.innerHTML = out.innerHTML + "3";
-    } else if (click_id == "four") {
-        out.innerHTML = out.innerHTML + "4";
-    } else if (click_id == "five") {
-        out.innerHTML = out.innerHTML + "5";
-    } else if (click_id == "six") {
-        out.innerHTML = out.innerHTML + "6";
-    } else if (click_id == "seven") {
-        out.innerHTML = out.innerHTML + "7";
-    } else if (click_id == "eight") {
-        out.innerHTML = out.innerHTML + "8";
-    } else if (click_id == "nine") {
-        out.innerHTML = out.innerHTML + "9";
+    } else if ((click_id == "one1") || (click_id == "two2") || (click_id == "three3")
+        || (click_id == "four4") || (click_id == "five5") || (click_id == "six6") ||
+        (click_id == "seven7") || (click_id == "eight8") || (click_id == "nine9")) {
+        print_nat(click_id);
     } else if (click_id == "zero") {
-        out.innerHTML = out.innerHTML + "0";
+        print_zero();
     } else if (click_id == "divi") {
-        if ((out.innerHTML == "") ||  (out.innerHTML == null)) {
-            out.innerHTML = "0";
-        }
+
         out.innerHTML = out.innerHTML + " / ";
     } else if (click_id == "multi") {
-        if ((out.innerHTML == "") ||  (out.innerHTML == null)) {
-            out.innerHTML = "0";
-        }
         out.innerHTML = out.innerHTML + " * ";
     } else if (click_id == "plus") {
-        if ((out.innerHTML == "") ||  (out.innerHTML == null)) {
-            out.innerHTML = "0";
-        }
         out.innerHTML = out.innerHTML + " + ";
     } else if (click_id == "sub") {
-        if ((out.innerHTML == "") ||  (out.innerHTML == null)) {
-            out.innerHTML = "0";
-        }
         out.innerHTML = out.innerHTML + " - ";
     } else {
         console.log("wrong button assigned")
